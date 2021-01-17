523747b269fe4e4dc0219ad9817b905b70faed99
Carla <carla@layc-dc.org>
2021-01-17 15:43:52 -0500

    updated table with new data to sort

--- my-app/src/components/Table.js
+++ my-app/src/components/Table.js
@@ -5,10 +5,12 @@ function Table() {
         <table class="table">
   <thead class="thead-dark">
     <tr>
-      <th scope="col">#</th>
+      <th scope="col">ID#</th>
       <th scope="col">First</th>
       <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Email</th>
+      <th scope="col">Title</th>
+      <th scope="col">Salary</th>
     </tr>
   </thead>
   <tbody>
@@ -16,19 +18,105 @@ function Table() {
       <th scope="row">1</th>
       <td>Mark</td>
       <td>Otto</td>
-      <td>@mdo</td>
+      <td>mdo@gmail</td>
+      <td>Manager</td>
+      <td>75,000</td>
     </tr>
     <tr>
       <th scope="row">2</th>
       <td>Jacob</td>
       <td>Thornton</td>
-      <td>@fat</td>
+      <td>jayT@yahoo.com</td>
+      <td>Instructor</td>
+      <td>45,000</td>
     </tr>
     <tr>
       <th scope="row">3</th>
       <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
+      <td>Wells</td>
+      <td>larrywell@gmail.com</td>
+      <td>Engineer</td>
+      <td>90,000</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Donna</td>
+      <td>Karen</td>
+      <td>donnak@aol.com</td>
+      <td>Designer</td>
+      <td>120,000</td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Caleb</td>
+      <td>Mann</td>
+      <td>calman@gmail.com</td>
+      <td>Salesman</td>
+      <td>60,000</td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>Jacob</td>
+      <td>Risch</td>
+      <td>cobj@gmail.com</td>
+      <td>Manager</td>
+      <td>90,000</td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>Lyle</td>
+      <td>Taylor</td>
+      <td>lytay@gmail.com</td>
+      <td>Instructor</td>
+      <td>49,000</td>
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>Selam</td>
+      <td>Negatu</td>
+      <td>selamneg@yahoo.com</td>
+      <td>Marketing Director</td>
+      <td>120,000</td>
+    </tr>
+    <tr>
+      <th scope="row">9</th>
+      <td>Shanna</td>
+      <td>Howard</td>
+      <td>shanhow@aol.com</td>
+      <td>Designer</td>
+      <td>125,000</td>
+    </tr>
+    <tr>
+      <th scope="row">10</th>
+      <td>Pedro</td>
+      <td>Enriquez</td>
+      <td>pedroe@yahoo.com</td>
+      <td>Writer</td>
+      <td>35,000</td>
+    </tr>
+    <tr>
+      <th scope="row">11</th>
+      <td>Brian</td>
+      <td>Mendes</td>
+      <td>bmen@aol.com</td>
+      <td>Education Director</td>
+      <td>65,000</td>
+    </tr>
+    <tr>
+      <th scope="row">12</th>
+      <td>Tyler</td>
+      <td>Taylor</td>
+      <td>tt@gmail.com</td>
+      <td>Engineer</td>
+      <td>90,000</td>
+    </tr>
+    <tr>
+      <th scope="row">13</th>
+      <td>Larry</td>
+      <td>Jacobs</td>
+      <td>jacobl@gmail.com</td>
+      <td>Writer</td>
+      <td>35,000</td>
     </tr>
   </tbody>
 </table>
