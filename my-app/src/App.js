a03c06a5d4a75e2fdf964651f4600c54535807f3
Carla <carla@layc-dc.org>
2021-01-24 19:35:41 -0500

    moved code into one App file, renamed class component, assigned new variable to array of objects and mapped over those values to create a table of employees

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,52 @@
 import React from "react";
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Table from "./components/Table";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
+// import EmployeeTable from "./src/components/EmployeeTable"
 
-function App() {
-  return (
-    <div className="jumbotron">
-      <p className="title">
-        Employee Directory
-      </p>
-      <Table />;
-    </div>
-  );
+
+// import API from "./utils/API";
+
+export default class App extends React.Component {
+
+  state = {
+    employees: []
+  }
+
+  componentDidMount() {
+    axios.get("https://randomuser.me/api?results=200&nat=us")
+      .then(res => {
+        const employees = res.data.results;
+        this.setState({ employees });
+      })
+  };
+
+  //Map over this.state.employees and render an EmployeeTable component for each employee object
+  // function App() {
+  render() {
+    return(
+      <div>
+      {this.state.employees.map(employee => (
+        <div>
+          <div>
+            {employee.name.first}
+          </div>
+          <div>
+            {employee.name.last}
+          </div>
+          <div>
+            {employee.telephone}
+          </div>
+          <div>
+            {employee.email}
+          </div>
+          <div>
+            {employee.dob.age}
+          </div>
+        </div>
+      ))}
+      </div>
+    )  
+ 
+  
+  }
 }
-
-export default App;
---
+++ my-app/src/components/EmployeeTable.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function EmployeeTable(props) {
+  return (
+   // <div className="text-center">
+    //   {props.results.map(item => (
+    //     <h3>{props.firstName}</h3>
+    //     <h3>{props.lastName}</h3>
+    //     <h3>{props.telephone}</h3>
+    //     <h3>{props.email}</h3>
+    //     <h3>{props.birthDate}</h3>
+  )
+}
+export default EmployeeTable;
